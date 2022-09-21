1009c9fe41617ec19784b316bb7725ee72280df1
cyc <ycpss91255@gmail.com>
2022-09-22 00:17:29 +0800

    fix : Homework for third work
    add LED right shift

---
+++ Qsys/software/test/obj/default/hello_world.d
@@ -0,0 +1,11 @@
+obj/default/hello_world.o: hello_world.c ../test_bsp/system.h \
+  ../test_bsp/linker.h ../test_bsp//HAL/inc/io.h \
+  ../test_bsp//HAL/inc/alt_types.h
+
+../test_bsp/system.h:
+
+../test_bsp/linker.h:
+
+../test_bsp//HAL/inc/io.h:
+
+../test_bsp//HAL/inc/alt_types.h:
